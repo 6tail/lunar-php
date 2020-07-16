2f6fe541596caddf31d63493816e7d11016557d3
6tail <6tail@6tail.cn>
2020-07-16 16:59:58 +0800

    v1.0.7 优化儒略日计算；使用最新寿星天文历算法修复节气误差；节气表中追加下一阴历年的冬至；增加时辰吉神方位。

--- demo.php
+++ demo.php
@@ -3,9 +3,37 @@ include 'lunar.php';
 
 use com\nlf\calendar\util\HolidayUtil;
 use com\nlf\calendar\Lunar;
+use com\nlf\calendar\Solar;
 
 $lunar = Lunar::fromYmd(1986,4,21);
 echo $lunar->toFullString()."\n";
 echo $lunar->getSolar()->toFullString()."\n";
 
+// 节假日
 echo HolidayUtil::getHoliday('2020-05-02')."\n";
+
+// 儒略日
+$solar = Solar::fromYmd(2020, 7, 15);
+echo $solar->getJulianDay()."\n";
+
+$solar = Solar::fromJulianDay(2459045.5);
+echo $solar->toFullString()."\n";
+
+// 节气表
+$lunar = Lunar::fromDate(new DateTime());
+$jieQi = $lunar->getJieQiTable();
+foreach ($jieQi as $key => $value) {
+  echo $key." = ".$value->toYmdHms()."\n";
+}
+
+echo $lunar->getTimePositionFu()."\n";
+echo $lunar->getTimePositionXi()."\n";
+echo $lunar->getTimePositionCai()."\n";
+echo $lunar->getTimePositionYinGui()."\n";
+echo $lunar->getTimePositionYangGui()."\n";
+
+echo $lunar->getTimePositionFuDesc()."\n";
+echo $lunar->getTimePositionXiDesc()."\n";
+echo $lunar->getTimePositionCaiDesc()."\n";
+echo $lunar->getTimePositionYinGuiDesc()."\n";
+echo $lunar->getTimePositionYangGuiDesc()."\n";
